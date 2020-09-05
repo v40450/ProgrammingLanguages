8e69bc1ba18a02ad0e659ca6066395772a2eb420
v40450@outlook.com <v40450@outlook.com>
2020-09-05 17:14:57 +0800

    Add project files.

---
+++ ProgrammingLanguages/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ProgrammingLanguages
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<Language> languages = File.ReadAllLines("./languages.tsv")
+              .Skip(1)
+              .Select(line => Language.FromTsv(line))
+              .ToList();
+
+            foreach (Language language in languages)
+            {
+                Console.WriteLine(language.Prettify());
+            }
+
+            var queryResult = from h in languages
+                              select $"{h.Year}, {h.Name}, {h.ChiefDeveloper}";
+
+            foreach (string h in queryResult)
+            {
+                Console.WriteLine(h);
+            }
+
+
+        }
+    }
+}
