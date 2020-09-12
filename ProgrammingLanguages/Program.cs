49ef81f01b881db4a5eea238bfe981b145001658
v40450@outlook.com <v40450@outlook.com>
2020-09-12 18:02:18 +0800

    Completed LINQ queries for programming languages project, added 2 method to print

--- ProgrammingLanguages/Program.cs
+++ ProgrammingLanguages/Program.cs
@@ -5,29 +5,75 @@ using System.Linq;
 
 namespace ProgrammingLanguages
 {
-    class Program
+  class Program
+  {
+    static void Main()
     {
-        static void Main()
-        {
-            List<Language> languages = File.ReadAllLines("./languages.tsv")
-              .Skip(1)
-              .Select(line => Language.FromTsv(line))
-              .ToList();
+      List<Language> languages = File.ReadAllLines("./languages.tsv")
+        .Skip(1)
+        .Select(line => Language.FromTsv(line))
+        .ToList();
+/*
+        //print all of the languages
+        PrettyPrintAll(languages);
 
-            foreach (Language language in languages)
-            {
-                Console.WriteLine(language.Prettify());
-            }
+        //a query that returns a string for each language
+        var queryResult = from h in languages
+          select $"{h.Year}, {h.Name}, {h.ChiefDeveloper}";
+        PrintAll(queryResult);
 
-            var queryResult = from h in languages
-                              select $"{h.Year}, {h.Name}, {h.ChiefDeveloper}";
+        //languages with the name "C#"
+        var cSharp = from h in languages
+          where h.Name == "C#"
+          select h;
+        PrettyPrintAll(cSharp);
 
-            foreach (string h in queryResult)
-            {
-                Console.WriteLine(h);
-            }
+        //languages with the chiefDeveloper Contains() "Microsoft"
+        var microsoft = from h in languages
+          where h.ChiefDeveloper.Contains("Microsoft")
+          select h;
+        fPrettyPrintAll(microsoft);
 
+        //languages with the Predecessors Contains() "Lisp"
+        var lisp = from h in languages
+          where h.Predecessors.Contains("Lisp")
+          select h;
+        PrettyPrintAll(lisp);
 
-        }
+        //languages names that Contains() "Script"
+        var script = from h in languages
+          where h.Name.Contains("Script")
+          select h.Name;
+        PrintAll(script);
+
+        //how many languages are included in the languages list?
+        Console.WriteLine(languages.Count);
+*/
+        //how many languages were launched between 1995 and 2005?
+        var launch = from h in languages
+          where (h.Year >= 1995) && (h.Year <= 2005)
+          select $"{h.Name} was invented in {h.Year}";
+        Console.WriteLine(launch.Count());
+
+        //print a string for each of those 66 languages
+        PrintAll(launch);
+       
     }
+
+    public static void PrettyPrintAll(IEnumerable<Language> langs)
+    {
+      foreach(var h in langs)
+      {
+        Console.WriteLine(h.Prettify());
+      }
+    }
+    public static void PrintAll(IEnumerable<Object> sequence)
+    {
+      foreach(var h in sequence)
+      {
+        Console.WriteLine(h);
+      }
+    }
+
+  }
 }
